e3938084c8a0b29d3a326c2c43065eb8c2ecd437
Sera <dev.sera20@gmail.com>
2020-03-01 12:30:15 +0300

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Sera on 3/1/20.
+//  Copyright © 2020 Sera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
